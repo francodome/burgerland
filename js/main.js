2b102bc3f683a5b3863f3d9bc4e1af73871c8d28
francodome <francodome4@gmail.com>
2023-10-05 18:59:42 -0300

    añadi archivo js para primer entrega

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/* ALGORITMO CON CONDICIONAL*/
+
+//let usuario1 = "Franco";
+//let usuario2 = "Andres";
+
+//let nombreUsuario = prompt("Ingrese su nombre de usuario");
+
+//if (nombreUsuario == usuario1 || nombreUsuario == usuario2) {//
+//    alert ("Bienvenido " + nombreUsuario);
+//} else { 
+//    alert("Nombre de usuario incorrecto");
+//} 
+
+
+/* ALGORITMO USANDO CICLO*/
+
+//for (let nombre = 1; nombre <= 50; nombre++) {
+//  let hamburguesa = prompt("Ingrese la hamburguesa que desea");
+    
+//   alert(` #${nombre} Hamburgesa: ${hamburguesa}`);
+//    }
+    
+//    alert("Nos quedamos sin stock, gracias por elegirnos!");
+
+
+/*SIMULADOR INTERACTIVO*/
+
+//const suma = (x, y) => x + y;
+//const resta = (x, y) => x - y;
+
+//let precioProducto = Number(prompt("Ingrese el precio del producto"));
+//let descuento = Number(prompt("Ingrese el descuento a aplicar"));
+
+//let nuevoPrecio = resta (precioProducto, descuento);
+
+//alert(nuevoPrecio);
